b4bb4d5a73f6ac286133036238b2a91b67116ddc
Rowan Taubitz <rowan@zantek.com.au>
2018-01-24 02:49:26 +1100

    Refactoring & Cleaning

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # AntilaX-3/smartexporter
 [smartexporter](https://github.com/AntilaX-3/docker-smartexporter) is a simple server that periodically scrapes S.M.A.R.T stats and exports them via HTTP for Prometheus consumption, written in Node.js.
+The attributes it supplies to Prometheus are configurable, as well as the labels it supplies. 
 ## Usage
 ```
 docker create --name=smartexporter \
@@ -15,5 +16,31 @@ The parameters are split into two halves, separated by a colon, the left hand si
 - `-p 9120` - http port for webserver
 
 It is based on alpine linux, utilising the official node docker repository with alpine tag, for shell access whilst the container is running do `docker exec -it smartexporter /bin/bash`.
+
+## Volumes
+
+The container uses a single volume mounted at '/config'. This volume stores the configuration file 'smartexporter.json'.
+
+    smartexporter
+    |-- smartexporter.json
+
+## Configuration
+
+The smartexporter.json is copied to the /config volume when first run. It has two optional parameters and one required. 
+
+The optional parameters are:
+ - port (default 9120)
+ - scrapeInterval (default 10 seconds)
+ 
+ The required parameter *reportedAttributes* is an array of objects. The objects define the SMART attributes that will be parsed and reported. The [default file](https://github.com/AntilaX-3/docker-smartexporter/blob/master/root/app/src/config/default.json) has examples. **Only one of either attributeID or attributeName is required.** 
+ 
+    attributeID: Number | The attribute ID
+    attributeName: String | The attribute name
+    name: String (Required) | The name reported to Prometheus, prepended with 'smartexporter_'
+    help: String (Required) | Help text provided to Prometheus
+    labelNames: Array of Strings | Mapped to data from the information section of smartctl. Can be used for labels, ie "Device" for /dev/sdx or "Serial Number" for the serial number of the HDD. 
+     
+[Known S.M.A.R.T. attributes (Wikipedia)](https://en.wikipedia.org/w/index.php?title=S.M.A.R.T.#Known_ATA_S.M.A.R.T._attributes)
 ## Version
-- **23/01/18:** Initial Release
+- **24/01/18:** 1.0.1 | Refactoring & Cleaning
+- **23/01/18:** 1.0.0 | Initial Release
--- root/app/src/index.js
+++ root/app/src/index.js
@@ -1,24 +1,25 @@
 import express from 'express';
+import fs from 'fs';
 import Prometheus from 'prom-client';
 import smartctl from 'smartctl';
-import fs from 'fs';
+
 import defConfig from './config/default';
 
-const app = express();
-
 // Attempt to read configuration file
-let metricsTimer;
 let config;
+let metricsTimer;
 
-const copyDefaultConfigFile = () => {
-  console.log('Unable to find configuration file, using defaults.');
+const copyDefaultConfigFile = (err, copyFile = false) => {
+  console.log(err);
   config = defConfig;
-  fs.writeFile('/config/smartexporter.json', `${JSON.stringify(defConfig, null, 2)}\n`, 'utf8', (err) => {
-    if (err) {
-      return console.log('Error writing to /config/smartexporter.json', err);
-    }
-    console.log('Copied default config to /config/smartexporter.json');
-  });
+  if (copyFile) {
+    fs.writeFile('/config/smartexporter.json', `${JSON.stringify(defConfig, null, 2)}\n`, 'utf8', (err) => {
+      if (err) {
+        return console.log('Error writing to /config/smartexporter.json', err);
+      }
+      console.log('Copied default config to /config/smartexporter.json');
+    });
+  }
 };
 
 if (fs.existsSync('/config/smartexporter.json')) {
@@ -26,15 +27,18 @@ if (fs.existsSync('/config/smartexporter.json')) {
   const fileContents = fs.readFileSync('/config/smartexporter.json', 'utf8');
 
   if (fileContents) {
-    config = JSON.parse(fileContents);
-  }
+    try {
+      config = JSON.parse(fileContents);
 
-  if (!config || !config.reportedAttributes) {
-    copyDefaultConfigFile();
+      if (!config || !config.attributes) {
+        copyDefaultConfigFile('Missing required in from the configuration file... Using defaults for now.');
+      }
+    } catch (err) {
+      copyDefaultConfigFile(`Unable to parse configuration file, please check JSON validity. Using defaults for now. Error: ${err}`);
+    }
   }
-
 } else {
-  copyDefaultConfigFile();
+  copyDefaultConfigFile('Unable to find configuration file, using defaults.', true);
 }
 
 const port = config.port || 9120;
@@ -42,12 +46,91 @@ const scrapeInterval = (config.scrapeInterval || 15) * 1000;
 const defaultMetrics = Prometheus.collectDefaultMetrics({ timeout: scrapeInterval });
 
 // Generate the prometheus metrics
-const prometheusMetrics = config.reportedAttributes.map((attribute) => {
-  const { name, help, labelNames } = attribute;
-  const reporter = new Prometheus.Gauge({ name: `smartexporter_${name}`, help, labelNames });
+const prometheusMetrics = config.attributes.map((attribute) => {
+  const { attributeName, attributeID, name, help, labelNames } = attribute;
+  // Sanitize inputs
+  attribute.attributeName = attributeName && attributeName.trim().replace(/ +/g, '_').toLowerCase();
+  attribute.attributeID = attributeID && Math.max(Math.min(attributeID, 255), 0);
+  attribute.name = name.trim().replace(/ +/g, '_');
+  attribute.labelNames = labelNames.map((labelName) => labelName.replace(/ +/g, '_').toLowerCase());
+
+  const reporter = new Prometheus.Gauge({
+    name: `smartexporter_${name.toLowerCase()}`,
+    help,
+    labelNames: attribute.labelNames,
+  });
+
   return { ...attribute, reporter };
 });
 
+const getMetrics = () => {
+  // Gather smartctl metrics
+
+  // Only care about /dev/sdx devices, this regex will be later used to test
+  // against the devices returned by the SMART scan
+  const devRegex = RegExp('\/dev\/s.+');
+
+  // Define asynchronous function
+  const gatherAsync = async () => {
+    // Perform --scan-open
+    const devices = await new Promise((res) => {
+      smartctl.scan((dev) => {
+        // Filter out strings that don't match the regex above
+        res(dev.filter(el => devRegex.test(el)));
+      });
+    });
+
+    // Get devices information
+    const getDeviceInfo = await Promise.all(devices.map(async (device) => {
+      return await new Promise((resolve, reject) => {
+        smartctl.info(device, (err, info) => {
+          if (err) return reject(err);
+          return resolve({ info: { device, ...info } });
+        });
+      });
+    }));
+
+    // Get devices attributes
+    const getDeviceAttributes = await Promise.all(getDeviceInfo.map(async (device) => {
+      return await new Promise((resolve, reject) => {
+        smartctl.smartAttrs(device.info.device, (err, info) => {
+          if (err) return reject(err);
+          return resolve({ ...device, attributes: info });
+        });
+      });
+    }));
+
+    // For each device concatenate info and append
+    for (let device of getDeviceAttributes) {
+      prometheusMetrics.forEach((metric) => {
+        // Bail if this metric isn't required
+        if (!metric || (!metric.attributeName && !metric.attributeID)) return;
+
+        // Find the attribute name in the array of attributes
+        const attribute = device.attributes.find((attribute) => attribute.id === metric.attributeID || attribute.attr === metric.attributeName);
+        if (attribute === undefined) return;
+
+        // Map label names to device info
+        const labels = {};
+        metric.labelNames.map((labelName) => {
+          labels[labelName] = device.info[labelName];
+        });
+
+        // Update the metric
+        metric.reporter.set(labels, attribute.raw);
+      });
+    }
+  };
+
+  // Invoke the async function
+  gatherAsync()
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+// HTTP Server
+const app = express();
 app.get('/', (req, res, next) => {
   setTimeout(() => {
     res.send('Point Prometheus here for your HDD statistics');
@@ -62,94 +145,17 @@ app.get('/metrics', (req, res) => {
 
 app.use((err, req, res, next) => {
   res.statusCode = 500;
-
-  // Dev only:
-  //res.json({ error: err.message });
   next();
 });
 
-const getMetrics = () => {
-  // Gather smartctl metrics
-
-  // We only care about /dev/sdx devices, this regex will be later used to test
-  // against the devices returned by the SMART scan
-  const devRegex = RegExp('\/dev\/s.+');
-
-  // deviceInfo is an array of our devices including their information
-  //const deviceInfo = [];
-
-  const gatherAsync = async () => {
-    // Perform --scan-open
-    const devices = await new Promise((res) => {
-      smartctl.scan((dev) => {
-        // Filter out strings that don't match the regex above
-        res(dev.filter(el => devRegex.test(el)));
-      });
-    });
-
-    // devices is an array of strings
-    const getDeviceInfo = await Promise.all(devices.map(async (device) => {
-      return await new Promise((resolve, reject) => {
-        smartctl.info(device, (err, info) => {
-          if (err) return reject(err);
-          return resolve({ info: { device, ...info } });
-        });
-      });
-    }));
-
-    const getDeviceAttributes = await Promise.all(getDeviceInfo.map(async (device) => {
-      return await new Promise((resolve, reject) => {
-        smartctl.smartAttrs(device.info.device, (err, info) => {
-          if (err) return reject(err);
-          return resolve({ ...device, attributes: info });
-        });
-      });
-    }));
-
-    // Get attributes info and append
-    for (let device of getDeviceAttributes) {
-      // console.log('Device:', device.device);
-      // console.log('Info:', device.info);
-      // console.log('Attributes:', device.attributes);
-
-      prometheusMetrics.forEach((metric) => {
-        if (!metric || !metric.attributeName) return;
-
-        // Find the attribute name in the array of attributes
-        const attribute = device.attributes.find((attribute) => attribute.attr === metric.attributeName);
-
-        if (attribute === undefined) return;
-
-        // console.log(`Metric ${metric.name} has a raw value of ${attribute.raw}`);
-
-        // Update the metric
-        const labels = {};
-
-        // Map label names to device info
-        metric.labelNames.map((labelName) => {
-          labels[labelName] = device.info[labelName];
-        });
-
-        metric.reporter.set(labels, attribute.raw);
-      });
-    }
-  };
-
-  gatherAsync()
-    .catch((err) => {
-      console.log(err);
-    });
-};
-
-
 const server = app.listen((port), () => {
-  console.log(`Running smartexporter. Listening on port ${port}.`);
-
   // Start a timer to fetch metrics
   metricsTimer = setInterval(getMetrics, scrapeInterval);
 
   // Get initial metrics
   getMetrics();
+
+  console.log(`Running smartexporter. Listening on port ${port}.`);
 });
 
 // Shutdown gracefully
